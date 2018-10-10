5e5b1b5e1cc802a29c88b881e49ef2860e925182
Lucas Bibiano <lucas.bibianot@gmail.com>
2018-10-10 10:11:58 -0300

    Adicionando a camada de persistência

---
+++ justebeer-app/src/main/java/br/com/lucasbibianot/jpa/producer/EntityManagerProducer.java
@@ -0,0 +1,35 @@
+package br.com.lucasbibianot.jpa.producer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.deltaspike.jpa.api.entitymanager.PersistenceUnitName;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+	@Inject
+    @PersistenceUnitName("justebeer-backend-pu")
+    private EntityManagerFactory entityManagerFactory;
+
+    @Produces
+    @Default
+    @RequestScoped
+    public EntityManager create()
+    {
+        return this.entityManagerFactory.createEntityManager();
+    }
+
+    public void dispose(@Disposes @Default EntityManager entityManager)
+    {
+        if (entityManager.isOpen())
+        {
+            entityManager.close();
+        }
+    }
+}
