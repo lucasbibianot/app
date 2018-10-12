9bbd8d3592b4eb1164ea5b24a1d0c3aa4d4e8e01
Lucas Bibiano <lucas.bibianot@gmail.com>
2018-10-11 23:03:39 -0300

    Listando todos os registros

--- justebeer-app/src/main/java/br/com/lucasbibianot/api/ExemploAPI.java
+++ justebeer-app/src/main/java/br/com/lucasbibianot/api/ExemploAPI.java
@@ -1,5 +1,8 @@
 package br.com.lucasbibianot.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -10,13 +13,14 @@ import javax.ws.rs.core.MediaType;
 
 import br.com.lucasbibianot.dao.ExemploDAO;
 import br.com.lucasbibianot.dto.ExemploDTO;
+import br.com.lucasbibianot.dto.ExemploEnvelopeDTO;
 import br.com.lucasbibianot.entidades.Exemplo;
 import br.com.lucasbibianot.security.annotations.JWTTokenNeeded;
 
 @Path("exemplo-api")
 @RequestScoped
 public class ExemploAPI {
-	
+
 	@Inject
 	private ExemploDAO dao;
 
@@ -24,7 +28,7 @@ public class ExemploAPI {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/exemplo/{codigo}")
 	@JWTTokenNeeded
-	public ExemploDTO GetPessoa(@PathParam("codigo") Long codigo) {
+	public ExemploDTO getExemplo(@PathParam("codigo") Long codigo) {
 
 		Exemplo entity = dao.getExemplo(codigo);
 
@@ -33,4 +37,23 @@ public class ExemploAPI {
 
 		return null;
 	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/exemplo")
+	@JWTTokenNeeded
+	public ExemploEnvelopeDTO getTodos() {
+		List<ExemploDTO> listaDtos = new ArrayList<>();
+		List<Exemplo> lista = dao.getTodos();
+
+		if (lista != null) {
+			for (Exemplo entity : lista) {
+				listaDtos.add(new ExemploDTO(entity.getId(), entity.getNome()));
+			}
+			
+			return new ExemploEnvelopeDTO(listaDtos);
+		}
+		return null;
+	}
+
 }
--- justebeer-app/src/main/java/br/com/lucasbibianot/dao/ExemploDAO.java
+++ justebeer-app/src/main/java/br/com/lucasbibianot/dao/ExemploDAO.java
@@ -1,5 +1,7 @@
 package br.com.lucasbibianot.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
@@ -8,12 +10,19 @@ import br.com.lucasbibianot.jpa.annotations.Repository;
 
 @Repository
 public class ExemploDAO {
-	
+
 	@Inject
 	private EntityManager entityManager;
 
 	public Exemplo getExemplo(Long codigo) {
 
 		return this.entityManager.find(Exemplo.class, codigo);
+	}
+
+	public List<Exemplo> getTodos() {
+		StringBuilder sql = new StringBuilder();
+		sql.append("select o From Exemplo o");
+
+		return this.entityManager.createQuery(sql.toString()).getResultList();
 	}
 }
